ef287f0a031891245b8be2f46a90358c2243dd0c
MarcosGual <marcos.gual@outlook.com>
2022-09-08 21:28:40 -0300

    conexion sql creada

--- pruebaControlesListas/src/pruebacontroleslistas/Persona.java
+++ pruebaControlesListas/src/pruebacontroleslistas/Persona.java
@@ -9,4 +9,25 @@ public class Persona {
         this.nombre = nombre;
         this.apellido = apellido;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    @Override
+    public String toString() {
+        return nombre + " " + apellido;
+    }
 }
